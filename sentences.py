17eb0bb1d0ff238565073a2feb41b6db3d223756
Vitor Barbosa <vitornga15@gmail.com>
2018-03-22 14:53:51 -0300

    Improve sentences and add regex to capture question

---
+++ sentences.py
@@ -0,0 +1,39 @@
+phrases = [
+    "pego meu carro e vou embora",
+    "tchau",
+    "oloco",
+    "veneza parece uma favela",
+    "cansei de ir pra europa",
+    "o carro e meu",
+]
+
+reply = [
+    "que que tao falando de mim",
+    "fala carai",
+    "fala",
+]
+
+responses = [
+    "sei la",
+    "parei de trabalhar como vidente",
+]
+
+questions = [
+    "como",
+    "qual",
+    "quando",
+    "quem",
+]
+
+yes_no = [
+    "sim",
+    "nao",
+    "nao po",
+]
+
+stop = [
+    "ta bom ne",
+    "pode parar de falar comigo ja ",
+    "cansei de falar com voce ",
+    "cansei de voce",
+]
